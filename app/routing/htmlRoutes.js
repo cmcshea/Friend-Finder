ced1b585f42a73cb414ef10234ec3a2ad55445f9
cmcshea <cory.mcshea@gmail.com>
2019-09-08 21:33:37 -0400

    adding html routes

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,13 @@
+//Dependencies 
+//Including the Path package to get correct file path for html
+
+var path = require("path");
+
+//Routing
+
+module.exports = function(app) {
+
+//HTML GET Requests
+
+
+}
--- server.js
+++ server.js
@@ -13,6 +13,10 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//Router - points the server to a series of route files
+
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
 
 
 //Starts the server to begin listening
