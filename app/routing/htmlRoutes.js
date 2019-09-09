687ff72a28e5ac80d8e08c0d586ae9dd7b7f7006
cmcshea <cory.mcshea@gmail.com>
2019-09-08 21:51:14 -0400

    pushing html route logic

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,9 +5,25 @@ var path = require("path");
 
 //Routing
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-//HTML GET Requests
+    //HTML GET Requests
 
+    //Displays the Home page
 
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+
+    //Displays the Survey page
+
+    app.get("/survey", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+
+    //If no matching route is found, default to Home
+
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
 }
